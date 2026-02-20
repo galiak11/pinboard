f966017f84197561b6b2f0a36456d0dc14646a33
yorilavi <yori80@hotmail.com>
2026-02-20 16:50:22 -0500

    E1: Creating a new xcode project

---
+++ PinBoard/PinBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PinBoard
+//
+//  Created by Galia on 2/20/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PinBoard/PinBoardTests/PinBoardTests.swift
@@ -0,0 +1,17 @@
+//
+//  PinBoardTests.swift
+//  PinBoardTests
+//
+//  Created by Galia on 2/20/26.
+//
+
+import Testing
+@testable import PinBoard
+
+struct PinBoardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
